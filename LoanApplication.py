d147c134d4ac2774306fd4187d9f7c17bca37a1d
Daniel Brenden <danielbrenden0gmail.com>
2019-05-26 09:17:33 -0600

    Create LoanApplication.py

---
+++ LoanApplication.py
@@ -0,0 +1,13 @@
+
+# This is a program to determine if you qualify for a loan based on income and credit score.
+
+income = input('What is your yearly income? ex. "50000" ')
+credit = int(input('What is your credit score? '))
+
+x = int(income)
+y = int(credit)
+
+if x >= 20000 or y >= 750:
+    print("You've qualified!")
+
+else: print("Sorry, you did not qualify.")
