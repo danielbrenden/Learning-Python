fbbfc7aae4c24ff87a6f533b2cc0509b30e0b34d
danielbrenden <danielbrenden0@gmail.com>
2019-09-01 10:01:49 -0600

    Added comments to show function of programs

--- RemoveDuplicatesFromList.py
+++ RemoveDuplicatesFromList.py
@@ -1,4 +1,4 @@
-# This program removes duplicates from a list of numbers via the append method.
+# This program removes duplicates from a list of numbers via the append method and prints the result.
 
 numbers = [2, 4, 5, 6, 5, 7, 8, 9, 9]
 unique_numbers = []
